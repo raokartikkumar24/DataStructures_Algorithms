2222f82bd8ab1fbd7ca39d29a28ce57280430575
Kartikkumar <krao24@gmail.com>
2014-12-22 12:44:47 +0530

    refactoring the code

--- Graphs/graph_bfs.cpp
+++ Graphs/graph_bfs.cpp
@@ -1,64 +1,8 @@
 #include<iostream>
 #include<vector>
 #include<queue>
-
+#include"graph.h"
 using namespace std;
-
-class CGraph
-{
-	
-public:
-	CGraph(int n,int e) : m_nNodes(n),m_nEdge(e){
-
-		cout<<"Creating graph object with "<<n<<" nodes"<<" and "<<e<<" m_vEdges"<<endl;
-		for (int i = 0; i < m_nNodes; ++i) {
-			std::vector<int> v;
-			m_vEdges.push_back(v);
-			}
-
-				
-
-	}
-
-	~CGraph(){}
-
-
-	void addEdges(int v, int u)
-	{
-		m_vEdges[v].push_back(u);
-		m_vEdges[u].push_back(v);
-	}
-
-
-	void display()
-	{
-
-		//cout<<"Size : "<<m_vEdges.size()<<endl;
-		for (int i = 0; i < m_nNodes; ++i)
-			for (int j = 0; j < m_vEdges[i].size(); ++j)
-				cout<<i<<"-"<<m_vEdges[i][j]<<endl;
-			
-
-	}
-
-	//TODO : The return should be a Iterable list
-	//Not sure what to use in C++
-	vector<int> adj(int v)
-	{
-
-		return m_vEdges[v];
-	}
-
-//TODO:Make this private and provide a public iterable function 
-//To access the adjacency
-		vector<vector<int>> m_vEdges;
-		int m_nNodes;
-		int m_nEdge;
-		
-
-
-};
-
 
 class CBFSTraversal
 {
