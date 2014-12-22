a4b286128d2b75672b2c82dbba39c14aae1e2e52
Kartikkumar <krao24@gmail.com>
2014-12-22 11:08:44 +0530

    removing the unneccessary code

--- Graphs/graph_adjacency.cpp
+++ Graphs/graph_adjacency.cpp
@@ -15,16 +15,6 @@ public:
 			m_vEdges.push_back(v);
 			}
 
-		m_pEdges = new int*[n];
-		for (int i = 0; i < n; ++i)
-			m_pEdges[i] = new int[n];
-
-		for (int i = 0; i < count; ++i)
-			for (int j = 0; j < count; ++j)
-				m_pEdges[i][j] = 0;
-		
-		
-
 	}
 
 	~CGraph(){}
@@ -34,8 +24,6 @@ public:
 	{
 		m_vEdges[v].push_back(u);
 		m_vEdges[u].push_back(v);
-		m_pEdges[v][u] = 1;
-		m_pEdges[u][v] = 1;
 	}
 
 
@@ -54,7 +42,7 @@ private:
 		vector<vector<int>> m_vEdges;
 		int m_nNodes;
 		int m_nEdge;
-		int **m_pEdges;
+		
 
 
 };
