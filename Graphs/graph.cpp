e35c2fdc00f3d407f5fe3f7f2e1d700cc33928ff
Kartikkumar <krao24@gmail.com>
2014-12-23 10:27:53 +0530

    Refactoring into .h and .cpp

---
+++ Graphs/graph.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+#include"graph.h"
+using namespace std;
+
+	CGraph::CGraph(int n,int e) : m_nNodes(n),m_nEdge(e){
+
+		cout<<"Creating graph object with "<<n<<" nodes"<<" and "<<e<<" m_vEdges"<<endl;
+		for (int i = 0; i < m_nNodes; ++i) {
+			std::vector<int> v;
+			CGraph::m_vEdges.push_back(v);
+			}
+
+				
+
+	}
+
+	CGraph::~CGraph(){}
+
+
+	void CGraph::addEdges(int v, int u)
+	{
+		m_vEdges[v].push_back(u);
+		m_vEdges[u].push_back(v);
+	}
+
+
+	void CGraph::display()
+	{
+
+		for (int i = 0; i < m_nNodes; ++i)
+			for (int j = 0; j < m_vEdges[i].size(); ++j)
+				cout<<i<<"-"<<m_vEdges[i][j]<<endl;
+			
+
+	}
+
+	//TODO : The return should be a Iterable list
+	//Not sure what to use in C++
+	vector<int> CGraph::adj(int v)
+	{
+
+		return m_vEdges[v];
+	}
+
+		
+		
+
+
